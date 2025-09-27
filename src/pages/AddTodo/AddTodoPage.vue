5566180648b06948aab6790b8fc93293d94b0c25
zelld <zeLLa444@gmail.com>
2025-09-27 18:46:48 +0700

    fix collor

--- src/pages/AddTodo/AddTodoPage.vue
+++ src/pages/AddTodo/AddTodoPage.vue
@@ -59,7 +59,7 @@ async function onSubmit() {
 
         <button
             type="submit"
-            class="inline-flex justify-center rounded-lg border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 dark:focus:ring-offset-background-dark"
+            class="inline-flex justify-center rounded-lg border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 dark:focus:ring-offset-background-dark"
         >
           Save
         </button>
